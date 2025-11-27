3f4ec0353596d6a410c13c9f5aaedc20c7abfea1
Brandon Smith <brandon.smith@cedana.ai>
2025-11-26 22:38:13 -0600

    fix: conceal check box

--- lua/linear-nvim/issue-view.lua
+++ lua/linear-nvim/issue-view.lua
@@ -257,7 +257,7 @@ function M.show_issue_in_buffer(issue, options)
                     local start_pos = actual_line:find("%- %[ %]")
                     vim.api.nvim_buf_set_extmark(buf, ui_ns_id, line_num, indent_len + start_pos - 1, {
                         end_col = indent_len + start_pos + 6,
-                        virt_text = {{"  □  ", "LinearMdTodo"}},
+                        virt_text = {{"□ ", "LinearMdTodo"}},
                         virt_text_pos = "overlay",
                         hl_mode = "combine",
                     })
@@ -267,7 +267,7 @@ function M.show_issue_in_buffer(issue, options)
                     local start_pos = actual_line:find("%- %[x%]")
                     vim.api.nvim_buf_set_extmark(buf, ui_ns_id, line_num, indent_len + start_pos - 1, {
                         end_col = indent_len + start_pos + 6,
-                        virt_text = {{"  ✔  ", "LinearMdDone"}},
+                        virt_text = {{"✔ ", "LinearMdDone"}},
                         virt_text_pos = "overlay",
                         hl_mode = "combine",
                     })
@@ -275,7 +275,7 @@ function M.show_issue_in_buffer(issue, options)
                     local start_pos = actual_line:find("%- %[X%]")
                     vim.api.nvim_buf_set_extmark(buf, ui_ns_id, line_num, indent_len + start_pos - 1, {
                         end_col = indent_len + start_pos + 6,
-                        virt_text = {{"  ✔  ", "LinearMdDone"}},
+                        virt_text = {{"✔ ", "LinearMdDone"}},
                         virt_text_pos = "overlay",
                         hl_mode = "combine",
                     })
